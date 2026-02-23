d1c2b1cc3add3f0a27183179684faae090011c35
Sewar77 <sewarjihad793@gmail.com>
2026-02-23 15:21:48 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(helmet())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-course-2026-1-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
