5ff57b17a2ec80bf64df770e31dc5836ee39caf2
Sewar77 <sewarjihad793@gmail.com>
2026-02-23 15:38:14 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -21,14 +21,7 @@ const allowedOrigins = [
     "https://mern-course-2026-1-frontend.onrender.com"];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = "The CORS policy for this site does not allow access from the specified Origin.";
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
