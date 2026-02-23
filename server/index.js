c381160ddfde55903f4bda09185ff30f728a391d
Sewar77 <sewarjihad793@gmail.com>
2026-02-23 15:04:06 +0300

    commit env

--- server/index.js
+++ server/index.js
@@ -32,5 +32,5 @@ app.use("/api", messagesRoutes)
 
 //error handlng middleware
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
